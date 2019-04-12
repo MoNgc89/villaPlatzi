63c39f977d978e9c97cb732a418d62a9f452b835
MoNgc89 <mgalindo.cruz@gmail.com>
2019-04-12 17:31:24 -0600

    commit con los nuevos animales que se multiplican

--- villaPlatzi.js
+++ villaPlatzi.js
@@ -1,47 +1,120 @@
-//Llamamos al canvas por ID y le damos el contexto 2d
 var vp = document.getElementById('villaplatzi')
 var papel = vp.getContext('2d')
 
-//Creamos una variable para guardar la ruta del fondo del canvas
-var mapa = 'tile.png'
+//Creamos nuevas funciones para los animales con un objeto literal
+//se agrega la url de la img y se agrega el elemento cargaOK que por default siempre está en false
+var fondo = {
+  url: 'tile.png',
+  cargaOK: false
+}
 
-//Creamos un nuevo objeto para guardar la clase de la img de fondo y mandarla a llamar
-//a las definiciones completas d eun objeto se les llama clase y se les asigna la palabra reservada new
-var fondo = new Image()
-fondo.src = mapa
+var vaca = {
+  url: 'vaca.png',
+  cargaOK: false
+}
 
-//se crea el evento para que cargue la imagen y se le pasa como parámetro junto con la función dibujar
-fondo.addEventListener('load', dibujar)
+var pollo = {
+  url: 'pollo.png',
+  cargaOK: false
+}
 
-//Se declara la función dibujar sobre el papel (ya que es el nombre del canvas)
-//En seguida a la funcion de dibujar en el papel se le llama la imagen en png (ya cargada-load) y los parametros para la posición en x y en y
+var cerdo = {
+  url: 'cerdo.png',
+  cargaOK: false
+}
+
+var mago = {
+  url: 'mago.png',
+  cargaOK: false
+}
+
+//Creamos nuevas funciones para la carga de las distintas imágenes
+fondo.imagen = new Image()
+fondo.imagen.src = fondo.url
+fondo.imagen.addEventListener('load', cargarFondo)
+
+// Se cambia la var de vaca
+vaca.imagen = new Image()
+vaca.imagen.src = vaca.url
+vaca.imagen.addEventListener('load', cargarVacas)
+
+pollo.imagen = new Image()
+pollo.imagen.src = pollo.url
+pollo.imagen.addEventListener('load', cargarPollos)
+
+cerdo.imagen = new Image()
+cerdo.imagen.src = cerdo.url
+cerdo.imagen.addEventListener('load', cargarCerdos)
+
+mago.imagen = new Image()
+mago.imagen.src = mago.url
+mago.imagen.addEventListener('load', cargarMago)
+
+//Se crea la funcion de la carga del fondo y se llama cuando sea true
+//Se llama al a funcion dibujar
+function cargarFondo(){
+  fondo.cargaOK = true
+  dibujar()
+}
+
+function cargarVacas(){
+  vaca.cargaOK = true
+  dibujar()
+}
+
+function cargarPollos(){
+  pollo.cargaOK = true
+  dibujar()
+}
+
+function cargarCerdos(){
+  cerdo.cargaOK = true
+  dibujar()
+}
+
+function cargarMago(){
+  mago.cargaOK = true
+  dibujar()
+}
+
+//Para que el codigo sepa cuando está cargada la imagen, se le manda un condicional para que realice la funcion
 function dibujar(){
-  papel.drawImage(fondo, 0, 0)
+if (fondo.cargaOK){
+  papel.drawImage(fondo.imagen, 0, 0)
+}
+if (vaca.cargaOK){
+  //creamos un ciclo for para agregar vacas cada vez que se recarga
+//var v es = a 0; mientras v sea menor que 5; entonces súmale un vaca
+  for(var v = 0; v < 5; v++){
+    var x = aleatorio (5, 420)
+    var y = aleatorio (5, 420)
+    papel.drawImage(vaca.imagen, x, y)
+  }
+}
+if (cerdo.cargaOK){
+  for(var v = 0; v < 4; v++){
+    var x = aleatorio (5, 300)
+    var y = aleatorio (5, 300)
+    papel.drawImage(cerdo.imagen, x, y)
+  }
+}
+if (pollo.cargaOK){
+  for(var v = 0; v < 5; v++){
+    var x = aleatorio (50, 350)
+    var y = aleatorio (50, 350)
+    papel.drawImage(pollo.imagen, x, y)
+  }
+}
+if (mago.cargaOK){
+  for(var v = 0; v < 1; v++){
+    papel.drawImage(mago.imagen, 450, 458)
+  }
+}
 }
 
-//Luego se declaran las funciones para dibujar cada animal
-//pero debido a que cada imagen carga en diferente tiempo, puede que se dibujen prmero los animales y arriba de ellos el mapa (tile)
-function dibujarVacas(){
-  papel.drawImage(vaca, 15, 25)
+//Definimos aleatorio que recibe dos parametros
+function aleatorio(min, max){
+  var resultado
+  resultado = Math.floor(Math.random() * (max-min +1)) + min
+  return resultado
 }
-
-function dibujarCerdos(){
-  papel.drawImage(cerdo, 10, 250)
-}
-
-function dibujarPollos(){
-  papel.drawImage(pollo, 105, 350)
-}
-
-//Después se crean las variables que van a contener las rutas de imagen y los event listeners de cada animal
-var vaca = new Image()
-vaca.src = 'vaca.png'
-vaca.addEventListener('load', dibujarVacas)
-
-var cerdo = new Image()
-cerdo.src = 'cerdo.png'
-cerdo.addEventListener('load', dibujarCerdos)
-
-var pollo = new Image()
-pollo.src = 'pollo.png'
-pollo.addEventListener('load', dibujarPollos)
